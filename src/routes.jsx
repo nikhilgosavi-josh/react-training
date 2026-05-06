58aad55426c75352e3dfda9dcafd24eae2853b38
Nikhil Gosavi <nikhilgosavi188@gmail.com>
2026-05-06 17:49:28 +0530

    Todo assignment

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Todo from "./assignments/assignment1/todo.jsx";
+// import Assignment2 from "./assignments/assignment2/Assignment2";
 
 function AppRoutes() {
 
     return (
         <Routes>
-            <Route path="/" element={<h2>Assignments Home</h2>} />
+            <Route path="/" element={<Navigate to="/todo" />} />
+            <Route path="/todo" element={<Todo />} />
+            {/* Add more assignments here */}
         </Routes>
     );
 }
