b4a1fe07dd9aab7ff9fc68d886538047389755d0
nikhilgosavi-josh <nikhil.gosavi@joshsoftware.com>
2026-06-10 11:14:49 +0530

    assignment8

---
+++ src/assignments/assignment8/TodoReduxTypescript/src/constants.ts
@@ -0,0 +1,7 @@
+import type { Todo } from "./types";
+
+export const initialTodos: Todo[] = [
+    { id: "1", todo: "Task 1", completed: false },
+    { id: "2", todo: "Task 2", completed: false },
+    { id: "3", todo: "Task 3", completed: false },
+];
